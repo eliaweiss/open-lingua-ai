d9061b7a4d48cb9265a9067f20584fc6ca02619e
eliaweiss <elia.weiss@gmail.com>
2024-07-19 11:29:00 -0300

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,16 +14,16 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <AppProvider {...{ isMenuOpen, setIsMenuOpen }}>
-          <div className="flex min-h-screen">
+          <div className="flex min-h-screen text-white">
             <aside
-              className={`fixed top-0 left-0 h-full bg-gray-800 text-white transition-all duration-300 ${
+              className={`fixed top-0 left-0 h-full bg-black text-white transition-all duration-300 ${
                 isMenuOpen ? "w-64" : "w-0"
               } md:w-64`}
             >
               <NavMenu isMenuOpen={isMenuOpen} />
             </aside>
-            <main className="flex-1 ml-0 md:ml-64">
-              <header className="p-4 bg-gray-200">
+            <main className="flex-1 ml-0 md:ml-64 bg-[#1a1a1e]">
+              <header className="p-4 bg-[#2b2a2aaa]">
                 {!isMenuOpen && (
                   <button
                     onClick={() => setIsMenuOpen(true)}
@@ -55,7 +55,7 @@ function NavMenu({}) {
         <button onClick={() => setIsMenuOpen(false)} className="md:hidden p-4">
           <XMarkIcon className="h-6 w-6" /> {/* Close icon */}
         </button>
-        <ul>
+        <ul className="p-4 ">
           <li>Exercise 1</li>
           <li>Exercise 2</li>
           <li>Settings</li>
