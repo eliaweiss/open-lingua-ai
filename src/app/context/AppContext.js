24417919d7fef05a5aa4c3029f184929a891797b
eliaweiss <elia.weiss@gmail.com>
2024-07-19 10:55:54 -0300

    1

---
+++ src/app/context/AppContext.js
@@ -0,0 +1,32 @@
+import { createContext, useState, useContext, useEffect } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [exercises, setExercises] = useState([]);
+
+  const saveExercise = (exercise) => {
+    setExercises((prevExercises) => {
+      const updatedExercises = [...prevExercises, exercise];
+      localStorage.setItem("exercises", JSON.stringify(updatedExercises));
+      return updatedExercises;
+    });
+  };
+
+  const loadExercises = () => {
+    const storedExercises = JSON.parse(localStorage.getItem("exercises")) || [];
+    setExercises(storedExercises);
+  };
+
+  useEffect(() => {
+    loadExercises();
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ exercises, saveExercise, loadExercises }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = () => useContext(AppContext);
