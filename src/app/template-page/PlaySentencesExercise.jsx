d3f31a3e41ae639c5778212ab90060a95a576c0c
eliaweiss <elia.weiss@gmail.com>
2024-07-19 13:13:22 -0300

    1

--- src/app/template-page/PlaySentencesExercise.jsx
+++ src/app/template-page/PlaySentencesExercise.jsx
@@ -2,5 +2,9 @@ import { usePlaySentenceContext } from "./context/PlaySentenceContext";
 
 export const PlaySentencesExercise = () => {
   const { t } = usePlaySentenceContext();
-  return <div>Not implemented</div>;
+  return (
+    <div className="flex flex-col justify-center items-center text-center w-full">
+      <div>Not implemented</div>
+    </div>
+  );
 };
