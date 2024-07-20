e0ec62ae94eaef752404f2faaf41aea72923f4a0
eliaweiss <elia.weiss@gmail.com>
2024-07-20 13:54:41 -0300

    init storage

--- src/app/context/AppContext.js
+++ src/app/context/AppContext.js
@@ -10,39 +10,65 @@ const LANGUAGE = {
 const todayStartTime = () => new Date().setHours(0, 0, 0, 0); // Midnight of the current day
 
 export const AppProvider = ({ children }) => {
-  const [phraseRange, setPhraseRange] = useState([1, 1000]); // Example range for phrases
-
+  const [phraseRange, setPhraseRange] = useState([1, 1000]);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [theme, setTheme] = useState(
-    typeof window === "undefined"
-      ? "light"
-      : localStorage.getItem("theme", "light")
-  );
+  const [theme, setTheme] = useState("light");
   const [phrases, setPhrases] = useState([]);
-  const [sourceLanguage, setSourceLanguage] = useState(
-    myLocalStorage.get("sourceLanguage", LANGUAGE.EN_US)
-  );
-  const [targetLanguage, setTargetLanguage] = useState(
-    myLocalStorage.get("targetLanguage", LANGUAGE.PT_BR)
-  );
-  const [sourceLanguageRate, setSourceLanguageRate] = useState(
-    myLocalStorage.get("sourceLanguageRate", 1)
-  );
-  const [targetLanguageRate, setTargetLanguageRate] = useState(
-    myLocalStorage.get("targetLanguageRate", 1)
-  );
+  const [sourceLanguage, setSourceLanguage] = useState(LANGUAGE.EN_US);
+  const [targetLanguage, setTargetLanguage] = useState(LANGUAGE.PT_BR);
+  const [sourceLanguageRate, setSourceLanguageRate] = useState(1);
+  const [targetLanguageRate, setTargetLanguageRate] = useState(1);
   const [isSrcRtl, setIsSrcRtl] = useState(false);
   const [isTargetRtl, setIsTargetRtl] = useState(false);
-
-  ////////////////////////////////////////////////////////////////
-  // daily count
-  const [dailyCount, setDailyCount] = useState(
-    myLocalStorage.get("dailyCount", 0)
-  );
-
+  const [dailyCount, setDailyCount] = useState(0);
   const [currentDaytimeStamp, setCurrentDaytimeStamp] = useState(
-    myLocalStorage.get("currentDaytimeStamp", todayStartTime())
+    todayStartTime()
   );
+
+  useEffect(() => {
+    const initializeState = async () => {
+      const storedPhraseRange = await storage.get("phraseRange", [1, 1000]);
+      setPhraseRange(storedPhraseRange);
+
+      const storedTheme = localStorage.getItem("theme", "light");
+      setTheme(storedTheme);
+
+      const storedSourceLanguage = await storage.get(
+        "sourceLanguage",
+        LANGUAGE.EN_US
+      );
+      setSourceLanguage(storedSourceLanguage);
+
+      const storedTargetLanguage = await storage.get(
+        "targetLanguage",
+        LANGUAGE.PT_BR
+      );
+      setTargetLanguage(storedTargetLanguage);
+
+      const storedSourceLanguageRate = await storage.get(
+        "sourceLanguageRate",
+        1
+      );
+      setSourceLanguageRate(storedSourceLanguageRate);
+
+      const storedTargetLanguageRate = await storage.get(
+        "targetLanguageRate",
+        1
+      );
+      setTargetLanguageRate(storedTargetLanguageRate);
+
+      const storedDailyCount = await storage.get("dailyCount", 0);
+      setDailyCount(storedDailyCount);
+
+      const storedCurrentDaytimeStamp = await storage.get(
+        "currentDaytimeStamp",
+        todayStartTime()
+      );
+      setCurrentDaytimeStamp(storedCurrentDaytimeStamp);
+    };
+
+    initializeState();
+  }, []);
 
   // Function to check if the stored timestamp is for today
   const isSameDay = (timestamp1, timestamp2) => {
@@ -58,15 +84,15 @@ export const AppProvider = ({ children }) => {
       // Reset the count if the current timestamp is not the same day as stored timestamp
       setDailyCount(0);
       setCurrentDaytimeStamp(todayTimestamp);
-      myLocalStorage.set("dailyCount", 0);
-      myLocalStorage.set("currentDaytimeStamp", todayTimestamp);
+      storage.set("dailyCount", 0);
+      storage.set("currentDaytimeStamp", todayTimestamp);
     }
   }, [currentDaytimeStamp]);
 
   const incrDailyCount = () => {
     const newCount = dailyCount + 1;
     setDailyCount(newCount);
-    myLocalStorage.set("dailyCount", newCount);
+    storage.set("dailyCount", newCount);
   };
 
   const loadPhrase = async () => {
@@ -101,6 +127,9 @@ export const AppProvider = ({ children }) => {
     setTheme(storedTheme);
   }, []);
 
+  useEffect(() => {
+    storage.set("phraseRange", phraseRange);
+  }, [phraseRange]);
   return (
     <AppContext.Provider
       value={{
