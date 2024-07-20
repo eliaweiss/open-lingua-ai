8131149f4ec79a3fc67bd7145dc73e693f273225
eliaweiss <elia.weiss@gmail.com>
2024-07-20 13:26:42 -0300

    daily count

--- src/app/components/AppHeader.jsx
+++ src/app/components/AppHeader.jsx
@@ -1,8 +1,16 @@
 import { Bars3Icon } from "@heroicons/react/24/outline"; // Correct import syntax for v2
 import { useAppContext } from "../context/AppContext";
+import { useEffect, useState } from "react";
 
 export function AppHeader({}) {
   const { theme, isMenuOpen, setIsMenuOpen, dailyCount } = useAppContext();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null; // Avoid rendering mismatched content
 
   return (
     <div className="flex space-x-5">
