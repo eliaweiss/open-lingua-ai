92a5689e10222d05e154a5af0a1983ed58a684ca
Elia <elia.weiss@gmail.com>
2024-07-28 16:05:34 -0300

    translate main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,48 +1,52 @@
 "use client";
 
 import MenuItem from "./components/MenuItem";
+import { useTranslation } from "./i18n/useTranslation";
 
 export default function Home() {
+  const t = useTranslation();
   return (
     <div>
       <div className="flex flex-col justify-center lg:w-[60%] mx-auto">
-        <div className="text-lg font-bold">Exercise:</div>
-        <MenuItem href="/playSentences">Play Sentence Exercise</MenuItem>
-        <MenuItem href="/scramble">Scramble Exercise</MenuItem>
+        <div className="text-lg font-bold">{t("exercise_title")}:</div>
+        <MenuItem href="/playSentences">{t("play_sentence_exercise")}</MenuItem>
+        <MenuItem href="/scramble">{t("scramble_exercise")}</MenuItem>
         <div className="mt-10 font-semibold">
-          Welcome to{" "}
+          {t("welcome_to")}{" "}
           <span className="font-bold text-lg">Open-Lingua-AI.org</span>
         </div>
-        <div className="mt-4 ">
-          This is an open source project that allow you practice a new language
-          for free.
+        <div className="mt-4 ">{t("open_source_project_description")}</div>
+        <div className="mt-2 ">
+          {t("project_stage_description")}
+          <ul className="space-y-2 my-2">
+            <li>
+              <span className="font-bold">
+                {t("english_to_portuguese")}
+                {"->"}
+                {t("portuguese_to_english")}
+              </span>
+            </li>
+            <li>
+              <span className="font-bold">
+                {t("portuguese_to_english")}
+                {"->"}
+                {t("english_to_portuguese")}
+              </span>
+            </li>
+          </ul>
+          {t("support_any_language_pair")}
         </div>
         <div className="mt-2 ">
-          The project is in early development stage and only support{" "}
-          <span className="font-bold">
-            English
-            {"->"}Portuguese
-          </span>
-          , but it is meant to support any language pair using Language-Model
-          (LLM)
-        </div>
-        <div className="mt-2 ">
-          Please give it a try and{" "}
+          {t("please_give_it_a_try")}{" "}
           <a
             className="underline text-blue-500"
             href="mailto:elia.weiss@gmail.com"
           >
-            send me feedback{" "}
+            {t("send_me_feedback")}{" "}
           </a>
         </div>
-        <div className="mt-2 ">
-          We currently support only two exercises that are easy to understand.
-          Therefore, the explanation provided is brief. However, I plan to
-          elaborate on the text and the exercises in the future.
-        </div>
-        <div className="mt-2 ">
-          {`Note: The app does not store any information remotely. All data is stored locally, and there is no backup available.`}
-        </div>
+        <div className="mt-2 ">{t("current_support_exercises")}</div>
+        <div className="mt-2 ">{t("note_app_data_storage")}</div>
       </div>
     </div>
   );
