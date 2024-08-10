53f32358f6833bf0a7d0bd943dd81df3a94437c6
Elia <elia.weiss@gmail.com>
2024-08-10 16:03:47 -0300

    rtl

--- src/app/scramble/component/UserBufferDisplay.jsx
+++ src/app/scramble/component/UserBufferDisplay.jsx
@@ -5,13 +5,13 @@ export function UserBufferDisplay() {
   const { getCurrentUserBufferArray } = useScrambleContext();
   const { isTargetRtl } = useAppContext();
   return (
-    <div className="flex flex-wrap">
+    <div className={`flex flex-wrap ${isTargetRtl ? "text-right text-rtl" : "text-left"}`}>
       {getCurrentUserBufferArray().map((word, key) => (
         <span
           key={key}
           className={`inline mr-2 rounded-lg ${
             word.isHint ? "bg-[#89090945]" : ""
-          } ${isTargetRtl ? "text-right text-rtl" : "text-left"}`}
+          }`}
         >
           {word.txt}
         </span>
