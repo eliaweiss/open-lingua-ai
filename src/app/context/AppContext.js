52e2bbb2da061792382232d070d17f05ada74826
Elia <elia.weiss@gmail.com>
2024-08-30 18:48:29 -0300

    handleReverseLang

--- src/app/context/AppContext.js
+++ src/app/context/AppContext.js
@@ -37,6 +37,7 @@ export const AppProvider = ({ children }) => {
     availablePhraseTranslation,
     readSettingsArray,
     locale,
+    handleReverseLang,
   } = useAppStore();
 
   const initFlagRef = useRef(false);
