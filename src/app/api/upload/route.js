499d45db1cfe7bb89993793a7c3c424e9c554d04
Elia <elia.weiss@gmail.com>
2024-09-01 10:59:43 -0300

    upload

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,25 +1,25 @@
-// import { NextResponse } from "next/server";
-// import { writeFile, mkdir } from "fs/promises";
-// import path from "path";
+import { NextResponse } from "next/server";
+import { writeFile, mkdir } from "fs/promises";
+import path from "path";
 
-// export async function POST(req) {
-//   try {
-//     const formData = await req.formData();
-//     const file = formData.get("file");
-//     if (!file) {
-//       return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
-//     }
+export async function POST(req) {
+  try {
+    const formData = await req.formData();
+    const file = formData.get("file");
+    if (!file) {
+      return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
+    }
 
-//     const buffer = Buffer.from(await file.arrayBuffer());
-//     const uploadDir = path.join(process.cwd(), "/tmp");
-//     const filePath = path.join(uploadDir, file.name);
+    const uploadDir = path.join(process.cwd(), "/tmp");
+    const filePath = path.join(uploadDir, file.name);
 
-//     await mkdir(uploadDir, { recursive: true });
-//     await writeFile(filePath, buffer);
+    // const buffer = Buffer.from(await file.arrayBuffer());
+    // await mkdir(uploadDir, { recursive: true });
+    // await writeFile(filePath, buffer);
 
-//     return NextResponse.json({ success: true, filePath });
-//   } catch (error) {
-//     console.error("Upload error:", error);
-//     return NextResponse.json({ error: "File upload failed" }, { status: 500 });
-//   }
-// }
+    return NextResponse.json({ success: true, filePath });
+  } catch (error) {
+    console.error("Upload error:", error);
+    return NextResponse.json({ error: "File upload failed" }, { status: 500 });
+  }
+}
