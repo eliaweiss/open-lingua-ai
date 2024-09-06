f2ed33d78b94255160663d3b06b0d43350046782
Elia <elia.weiss@gmail.com>
2024-09-06 17:06:11 -0300

    handleSubmit

--- src/app/exercises/conjugate/ConjugateExercise.jsx
+++ src/app/exercises/conjugate/ConjugateExercise.jsx
@@ -5,25 +5,33 @@ import HorizontalRule from "@/app/components/HorizontalRule";
 import { createConjugationApi } from "./createConjugationApi";
 import useConjugateExerciseStore from "./store/ConjugateExerciseStore";
 import useAppStore from "@/app/store/appStore";
+import { Input } from "@/app/components/Input";
 
 export const ConjugateExercise = () => {
-  const { appInitFlag } = useAppStore();
+  const { appInitFlag, isTargetRtl } = useAppStore();
   const t = useTranslation(); // Add this line
-  const { exerciseData, setExerciseData, exerciseIndex, setExerciseIndex } =
-    useConjugateExerciseStore();
+  const {
+    exerciseData,
+    setExerciseData,
+    exerciseIndex,
+    setExerciseIndex,
+    answer,
+    setAnswer,
+    showCorrectAnswer,
+    setShowCorrectAnswer,
+  } = useConjugateExerciseStore();
 
   const currentExercise = useMemo(() => {
     if (!exerciseData) return null;
     return exerciseData[exerciseIndex];
   }, [exerciseData, exerciseIndex]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = () => {
+    setShowCorrectAnswer(true);
+    // e.preventDefault();
     // Add logic to check the answer
-    console.log("Submitted:", { verb, tense, answer });
+    // console.log("Submitted:", { verb, tense, answer });
   };
-
-
 
   return (
     <div>
@@ -38,9 +46,38 @@ export const ConjugateExercise = () => {
       <HorizontalRule />
       {currentExercise && (
         <div>
-          <div>{currentExercise.verb}</div>
-          <div>{currentExercise.tense}</div>
-          <div>{currentExercise.answer}</div>
+          {/* Exercise Panel */}
+          <div className="flex flex-col space-y-4 mt-4 text-2xl">
+            <div
+              className={`text-sText ${
+                isTargetRtl ? "text-right text-rtl" : "text-left"
+              }`}
+            >
+              {currentExercise.exercise}
+            </div>
+          </div>
+          <Input
+            value={answer}
+            onChange={(e) => setAnswer(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleSubmit(e);
+              }
+            }}
+            placeholder="Enter your answer"
+          />
+          {/* {showHint && (
+            <div className="text-sText text-2xl text-left fixed top-10 bg-pBg p-2 border border-pBorder rounded-sm ">
+              {suggestedTranslatedText}
+            </div>
+          )} */}
+          <div className="">Verb: {currentExercise.verb}</div>
+          <div className="">Tense: {currentExercise.tense}</div>
+          {showCorrectAnswer && (
+            <div className="">
+              Correct Answer: {currentExercise.completeSentence}
+            </div>
+          )}
         </div>
       )}
     </div>
--- src/app/exercises/conjugate/store/ConjugateExerciseStore.js
+++ src/app/exercises/conjugate/store/ConjugateExerciseStore.js
@@ -2,17 +2,25 @@ import { create } from "zustand";
 
 const useConjugateExerciseStore = create((set, get) => ({
   // State
-  exerciseData: null,
+  exerciseData: [],
   setExerciseData: (data) => set({ exerciseData: data }),
   exerciseIndex: 0,
   setExerciseIndex: (index) => set({ exerciseIndex: index }),
+  answer: "",
+  setAnswer: (answer) => set({ answer: answer }),
+  showCorrectAnswer: false,
+  setShowCorrectAnswer: (show) => set({ showCorrectAnswer: show }),
 
   getCurrentExercise: () => {
     const { exerciseData, exerciseIndex } = get();
     return exerciseData[exerciseIndex];
   },
 
-  resetExercise: () => set({ exerciseData: null, exerciseIndex: 0 }),
+  resetExercise: () =>
+    set({
+      answer: "",
+      showCorrectAnswer: false,
+    }),
 }));
 
 export default useConjugateExerciseStore;
