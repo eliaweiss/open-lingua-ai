15c555c0de6f2e895fef1f18862e8bca0c028db4
Elia <elia.weiss@gmail.com>
2024-09-07 16:10:11 -0300

    checkUserTranslate prompt

--- src/app/exercises/translate/checkUserTranslate.js
+++ src/app/exercises/translate/checkUserTranslate.js
@@ -29,6 +29,11 @@ If not, please correct the translation and provide explanation.
 If the translation is correct, please say "Correct".
 Please don't mention the teacher suggested translation - it is only for reference.
 Don't mention the term 'user' - refer to the user as 'you'.
+
+NOTE: 
+- if the Original text can be understood both as female or male, than accept both as correct translation.
+- if the Suggested teacher translation text contain none ascii characters, and the user translate contain the equivalent ascii characters 
+and other wise correct, than accept the translation as correct.
 `;
   return userMsg;
 }
